79be3a2dfcc871aab1b896532ca2691386c1b812
lavatee <aleksgraznov0@gmail.com>
2025-04-24 22:57:29 +0700

    pdf downloading was completed

--- js/pdf.js
+++ js/pdf.js
@@ -1,2 +1,9 @@
-const pdf = document.getElementById("pdf")
-pdf.src = localStorage.getItem("pdf")
+
+document.getElementById('downloadBtn').addEventListener('click', () => {
+    const link = document.createElement('a');
+    link.href = `https://raw.githubusercontent.com/lavatee/sever_frontend/refs/heads/main/pdf/${localStorage.getItem("pdf").split("pdf/")[1]}`
+    link.download = ""
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
