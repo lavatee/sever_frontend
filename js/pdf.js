425c59fc8b078e14588052c9cf928cdab312e2c3
lavatee <aleksgraznov0@gmail.com>
2025-04-24 23:19:45 +0700

    h2 text align was changed

--- js/pdf.js
+++ js/pdf.js
@@ -1,9 +1,2 @@
-
-document.getElementById('downloadBtn').addEventListener('click', () => {
-    const link = document.createElement('a');
-    link.href = `https://raw.githubusercontent.com/lavatee/sever_frontend/refs/heads/main/pdf/${localStorage.getItem("pdf").split("pdf/")[1]}`
-    link.download = ""
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-});
+const pdf = document.getElementById("pdf")
+pdf.src = localStorage.getItem("pdf")
